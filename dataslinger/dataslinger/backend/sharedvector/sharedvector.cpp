472055b1da80f59b65b1adf5d42b68875c77a870
sam <samcodesit@gmail.com>
2018-11-15 00:51:48 +0000

    Thinking about how to write shared memory vector implementation (condition variable would be cleaner than polling, but might just poll for all backends for consistency)

--- dataslinger/dataslinger/backend/sharedvector/sharedvector.cpp
+++ dataslinger/dataslinger/backend/sharedvector/sharedvector.cpp
@@ -7,7 +7,6 @@
 #include <boost/interprocess/allocators/allocator.hpp>
 #include <boost/interprocess/containers/vector.hpp>
 #include <boost/interprocess/sync/interprocess_mutex.hpp>
-#include <boost/interprocess/sync/interprocess_condition.hpp>
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/segment_manager.hpp>
 
@@ -27,7 +26,6 @@ public:
 
     boost::interprocess::vector<dataslinger::message::Message, boost::interprocess::allocator<dataslinger::message::Message, boost::interprocess::managed_shared_memory::segment_manager>> m_vector;
     boost::interprocess::interprocess_mutex mutex;
-    boost::interprocess::interprocess_condition cond_empty;
 };
 
 }
@@ -40,16 +38,10 @@ namespace sharedvector
 class SharedVector::SharedVectorImpl
 {
 public:
-    SharedVectorImpl(const std::function<void(const dataslinger::message::Message&)>& onReceive, const std::function<void(const dataslinger::event::Event&)>& onEvent, const dataslinger::connection::ConnectionOptions& info) : m_sharedData{nullptr}, m_onReceive{onReceive}, m_onEvent{onEvent}, m_info{info}
+    SharedVectorImpl(const std::function<void(const dataslinger::message::Message&)>& onReceive, const std::function<void(const dataslinger::event::Event&)>& onEvent, const dataslinger::connection::ConnectionOptions& info) : m_sendData{nullptr}, m_receiveData{nullptr}, m_onReceive{onReceive}, m_onEvent{onEvent}, m_info{info}
     {
         // Create a new segment with given name and size
-        boost::interprocess::managed_shared_memory segment(boost::interprocess::create_only, "MySharedMemory", 65536);
-
-        // Initialize shared memory STL-compatible allocator
-        //boost::interprocess::allocator<SharedData, boost::interprocess::managed_shared_memory::segment_manager> allocatorInstance();
-
-        // Construct shared data in shared memory
-        //m_sharedData = segment.construct<SharedData>("MySharedData")(allocatorInstance);
+        boost::interprocess::managed_shared_memory segment(boost::interprocess::open_or_create, "MySharedMemory", 65536);
     }
 
     ~SharedVectorImpl()
@@ -64,18 +56,34 @@ public:
 
     void run()
     {
+        // Initialize shared memory STL-compatible allocator
+        //boost::interprocess::allocator<SharedData, boost::interprocess::managed_shared_memory::segment_manager> allocatorInstance();
+
+        // Construct shared data in shared memory
+        //m_sendData = segment.find_or_construct<SharedData>("MySharedData")(allocatorInstance);
+
+        //m_receiveData = // TODO
     }
 
     void send(const dataslinger::message::Message& message)
     {
+        // TODO push to shared mem
     }
 
     void poll()
     {
+        // TODO grab mutex and read out/write in shared data
+        if(m_sendData) {
+
+        }
+        if(m_receiveData) {
+
+        }
     }
 
 private:
-    SharedData* m_sharedData;
+    SharedData* m_sendData; ///< Data being sent to other receivers - first come first served
+    SharedData* m_receiveData; ///< Data received from other clients
 
     const std::function<void(const dataslinger::message::Message&)> m_onReceive; ///< Callback triggered when the slinger receives a message
     const std::function<void(const dataslinger::event::Event&)> m_onEvent; ///< Callback triggered when the slinger produces an event
